99ce118ccf541ee75a05b291181e595f7a56352a
xsma-L <ismael.traore@epitech.eu>
2022-09-15 17:54:04 +0200

    Assets, Metadata, and CSS

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,12 +1,18 @@
+import Head from 'next/head';
 import Link from 'next/link';
+
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
             <h1>First Posts</h1>
             <h2>
                 <Link href='/'>Back to home</Link>
             </h2>
-        </>
+        </Layout>
     );
 }
